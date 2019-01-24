a8c62a8eea5803d7c6c6348604a852683a47a074
Ryan DeBose-Boyd <ryan96db@gmail.com>
2019-01-24 13:24:42 -0600

    Resize images, specials image depending on day of week, fix navigation bar links

--- js/script.js
+++ js/script.js
@@ -18,3 +18,29 @@ window.onclick = function(event) {
 		}
 	}
 }
+
+
+function insert() {
+	var src = document.getElementById("specialimage");
+	var img = document.createElement("img");
+	var date = new Date();
+              var dayofWeek = date.getDay();
+              
+              if (dayofWeek == 2) {
+              	img.src = "images/tacotuesdays2.jpg";
+              }
+
+              else if (dayofWeek == 3) {
+              	img.src = "images/margeritawednesday2.jpg";
+              }
+
+              else if (dayofWeek == 5) {
+               img.src = "images/fajitafriday.jpg";
+              }
+
+              else {
+              	img.src = "images/Los Mochis Picture2.jpg";
+              }
+	
+	src.appendChild(img);
+}
